4f7cc071932c951bc6949eed998c292534708ea7
amrutamadagum <amrutamagadum17222gmail.com>
2025-02-21 15:41:10 -0800

    done

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+const API_URL = "https://backendecommerce-5jmu.onrender.com";
 
 export async function login(credentials) {
   const response = await fetch(`${API_URL}/login`, {
